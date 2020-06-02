45a72a461e5037ee01d5694e448b63033e66c898
Shahzain Tariq <shahzaintariq.crpyto@gmail.com>
2020-06-02 16:56:34 +0500

    add some function

--- ERC721/property.sol
+++ ERC721/property.sol
@@ -8,36 +8,43 @@ contract property is ERC721{
     uint256 private _tokenId;
     uint256 private buyerID;
     
+    enum offer_approval{pending,approved,rejected}
+    
+    //details about property
     struct property_details{
         uint id;
         string property_address;
         uint acre;
         uint price;
+        address seller_addr;
         bool isOnSale;
     }
     
+    // details of buyer data
     struct buyer_data{
         uint id;
         address buyer_addr;
         uint buyer_offer;
+        offer_approval offer_request;
     }
     
-    buyer_data[] public buyer_array;
 
-    modifier property_owner(uint256 _token_id){
-        require(id_list[_token_id] == msg.sender,"ERROR: only property of owner can run this operation");
+    modifier property_owner(){
+        require(for_property[msg.sender].seller_addr == msg.sender,"ERROR: only property of owner can run this operation");
         _;
     }
     
-    mapping(address => property_details)  for_property;
-    mapping(uint256 => address) id_list;
-    mapping(uint256 => buyer_data[]) public buyer_list;
-    
+    mapping(address => property_details) public for_property;
+    mapping(uint256 => address) public id_list;
+    mapping(address => buyer_data) public buyer_list;
+    mapping(uint256 => buyer_data[]) public buyer_arr;
+    // mapping(uint256 => bool) public buyer_request;
     
     constructor() ERC721('Zain Town','SZT') public {
         
     }
     
+    //@dev this function register property and mint token of that protpert
     function register_property(string memory _propertyAddress, uint256 _acres_of_lane, uint256 _price_in_ether, string memory _tokenURi) public returns(bool){
         _tokenId++;
         uint256 tempId = _tokenId;
@@ -51,6 +58,7 @@ contract property is ERC721{
             property_address: _propertyAddress,
             acre: _acres_of_lane,
             price: _price_in_ether,
+            seller_addr: msg.sender,
             isOnSale: false
         });
         
@@ -59,18 +67,21 @@ contract property is ERC721{
         return true;
     }
     
-    function enable_property(uint token_or_Property_ID) public property_owner(token_or_Property_ID) returns(bool){
+    //function will enable property/token for sale
+    function enable_property(uint token_or_Property_ID) public property_owner() returns(bool){
         require(_exists(token_or_Property_ID),"ERROR: invailed token id");
         
         for_property[id_list[token_or_Property_ID]].isOnSale = true;
         return true;
     }
     
+    //function will return the price of property/token
     function price_of_property(uint token_or_Property_ID) public view returns(uint256){
         require(_exists(token_or_Property_ID),"ERROR: invailed token id");
         return for_property[id_list[token_or_Property_ID]].price;
     }
     
+    //this function will give offer aginst property for owner of property and add data buyer into contract
     function buy_request(uint token_or_Property_ID,uint _your_offer_inether) public returns(bool){
         require(_exists(token_or_Property_ID),"ERROR: invailed token id");
         require(for_property[id_list[token_or_Property_ID]].isOnSale, "ERROR: this property is currently not available for sale");
@@ -80,25 +91,63 @@ contract property is ERC721{
         temp_buyer_data = buyer_data({
             id: buyerID,
             buyer_addr: msg.sender,
-            buyer_offer: _your_offer_inether
+            buyer_offer: _your_offer_inether,
+            offer_request: offer_approval.pending
         }); 
         
-        buyer_array.push(temp_buyer_data);
-        
-        buyer_list[token_or_Property_ID].push(temp_buyer_data);
-        
+        buyer_arr[token_or_Property_ID].push(temp_buyer_data);
+    
+        buyer_list[msg.sender] = temp_buyer_data;
+        buyer_list[msg.sender].offer_request = offer_approval.pending ;
     }
     
-    
-    function check_offers(uint token_or_Property_ID) public view property_owner(token_or_Property_ID) returns(buyer_data[] memory){
+    //check offers of buyers for approval
+    function check_offers(uint token_or_Property_ID) public view property_owner returns(buyer_data[] memory){
         require(_exists(token_or_Property_ID),"ERROR: invailed token id");
         
         buyer_data[] memory arr;
-        arr = buyer_list[token_or_Property_ID];
+        arr = buyer_arr[token_or_Property_ID];
         return arr;
         
     }
     
+    //this function will accept the buyer offer request and buyer can buy the property
+    function accept_buyer_offer(address _buyer_address,uint token_or_Property_ID) public property_owner returns(bool){
+        require(_exists(token_or_Property_ID),"ERROR: invailed token id");
+        require(buyer_list[_buyer_address].buyer_addr == _buyer_address, "Error: invailid Buyers Address");
+        
+        buyer_list[_buyer_address].offer_request = offer_approval.approved;
+
+        return true;
+    }
+    
+    //this function will rejects the buyer offer request
+    function reject_buyers_offer(address _buyer_address,uint token_or_Property_ID) public property_owner returns(bool){
+        require(_exists(token_or_Property_ID),"ERROR: invailed token id");
+        require(buyer_list[_buyer_address].buyer_addr == _buyer_address, "Error: invailid Buyers Address");
+        
+        buyer_list[_buyer_address].offer_request = offer_approval.rejected;
+        
+        return true;
+    }
+    
+    //buyer who are approved from owner can buy the property with this function
+    function buy_property(uint256 token_or_Property_ID) public payable returns(bool){
+        require(_exists(token_or_Property_ID),"ERROR: invailed token id");
+        require(buyer_list[msg.sender].buyer_addr == msg.sender,"ERROR: you haven'y submited your buy request");
+        require(buyer_list[msg.sender].offer_request  ==  offer_approval.approved,"ERROR: your offer is not approved");
+        require(msg.value > 0,"ERROR: Ether not privided");
+        uint price_of = buyer_list[msg.sender].buyer_offer.mul(1*10**18);
+        require(price_of == msg.value, "ERROR: invailed price this price doesnot match with your given offer in buyer Request");
+    
+        address buyer_adrress = for_property[id_list[token_or_Property_ID]].seller_addr;
+        
+        _transfer(buyer_adrress,msg.sender,token_or_Property_ID);
+        
+         emit Transfer(buyer_adrress, msg.sender, token_or_Property_ID);
+    }
+    
+ 
     
 }
 
